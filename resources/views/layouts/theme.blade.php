0087de3a67d93607b2ae0f7d85ad8795674e8775
tanveershuvo <tanveershuvos@gmail.com>
2020-06-14 08:07:04 +0600

    Under Construction Message

--- resources/views/layouts/theme.blade.php
+++ resources/views/layouts/theme.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    {{-- <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> --}}
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="{{asset('assets/images/logo.png')}}" />
     <!-- CSRF Token -->
