679cc234f03ac14d80a8887779d82e86110da556
tanveershuvo <tanveershuvos@gmail.com>
2020-08-27 23:45:58 +0600

    NPM on heorku

--- resources/views/layouts/theme.blade.php
+++ resources/views/layouts/theme.blade.php
@@ -9,7 +9,6 @@
 <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ $title ?? 'JobPortal' }}</title>
-
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="{{asset('images/logo.png')}}"/>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
