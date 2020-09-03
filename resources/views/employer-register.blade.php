65949b1241970a3c1c396bb65e747fb667d14b63
tanveershuvo <tanveershuvos@gmail.com>
2020-09-03 17:24:37 +0600

    Employer Registration design

--- resources/views/employer-register.blade.php
+++ resources/views/employer-register.blade.php
@@ -2,38 +2,42 @@
 
 @section('content')
     <div class="container py-4">
-
-        <div class="d-flex border flex-md-row flex-column bg-white p-3">
-            <div class="mr-auto col-md-12">
-                <h3 class="mb-5 text-center">@lang('app.company_register')</h3>
+        <div class="d-flex register flex-md-row flex-column bg-white p-3">
+            <div class="col-md-12">
+                <h3 class="mb-5 text-center highlight">@lang('app.company_register')</h3>
                 <form method="POST" action="">
                     @csrf
-                    <h4 class="mb-3">Account Information</h4>
+                    <h4 class="mb-4">Account Information</h4>
                     <div class="form-row">
                         <div class="form-group col-md-4">
                             <label for="email">@lang('E-Mail Address')
                                 <span class="mendatory-mark">*</span></label>
                             <input id="email" type="email"
                                    class="form-control @error('email') is-invalid @enderror"
-                                   name="email" value="{{ old('email') }}">
+                                   name="email" placeholder="Type Email Address" value="{{ old('email') }}">
                             @error('email')
                             <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
                             @enderror
                         </div>
-
                         <div class="form-group col-md-4">
-                            <label for="password">@lang('Password')
-                                <span class="mendatory-mark">*</span></label>
-                            <input id="password" type="password"
-                                   class="form-control @error('password') is-invalid @enderror"
-                                   name="password">
+                            <label for="password">@lang('app.password') <span class="mendatory-mark">*</span></label>
+                            <div class="input-group">
+                                <input type="password" id="password"
+                                       class="form-control @error('password') is-invalid @enderror" name="password">
+                                <div class="input-group-append" id="icon-click">
+                                    <div class="input-group-text"><i class="fa fa-eye"></i></div>
+                                </div>
+                            </div>
+                            <small id="passwordHelpBlock" class="form-text text-muted">
+                                Password must contain minimum 8 characters with letters and numbers.
+                            </small>
 
                             @error('password')
                             <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
+                            <strong>{{ $message }}</strong>
+                        </span>
                             @enderror
                         </div>
 
@@ -44,19 +48,16 @@
                                    name="password_confirmation">
                         </div>
                     </div>
-                    <hr>
-                    <h4 class="mb-3">Company Details Information</h4>
-                    <div class="form-row">
 
+                    <h4 class="mb-4">Company Details Information</h4>
+                    <div class="form-row">
                         <div class="form-group col-md-6">
                             <label for="company_name">@lang('app.company') Name
                                 <span class="mendatory-mark">*</span></label>
-
                             <input id="company_name" type="text"
                                    class="form-control @error('company_name') is-invalid @enderror "
-                                   name="company_name"
+                                   name="company_name" placeholder="Type company name"
                                    value="{{ old('company_name') }}" autofocus>
-
                             @error('company_name')
                             <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -65,9 +66,8 @@
                         </div>
 
                         <div class="form-group col-md-6">
-                            <label for="company_name">Industry Type
+                            <label for="category">Industry Type
                                 <span class="mendatory-mark">*</span></label>
-
                             <select class="form-control" name="category" id="category">
                                 <option value="">@lang('app.select_category')</option>
                                 @foreach($categories as $category)
@@ -75,97 +75,127 @@
                                         {{$category->category_name}}</option>
                                 @endforeach
                             </select>
-
                         </div>
                     </div>
-                    <div class="form-group row">
-                        <label for="phone"
-                               class="col-md-4 col-form-label text-md-right">@lang('app.phone') <span
-                                class="mendatory-mark">*</span></label>
-                        <div class="col-md-6">
-                            <input id="phone" type="text"
-                                   class="form-control @error('phone') is-invalid @enderror"
-                                   name="phone" value="{{ old('phone') }}" autofocus>
-
-                            @error('phone')
-                            <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label for="address"
-                               class="col-md-4 col-form-label text-md-right">@lang('app.address')
-                            <span class="mendatory-mark">*</span></label>
-                        <div class="col-md-6">
-                            <input id="address" type="text"
-                                   class="form-control @error('address') is-invalid @enderror"
-                                   name="address"
-                                   value="{{ old('address') }}" autofocus>
-
-                            @error('address')
-                            <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-                    </div>
-
-
-                    <div class="form-group row">
-                        <label for="address_2"
-                               class="col-md-4 col-form-label text-md-right">@lang('app.address_2')</label>
-                        <div class="col-md-6">
-                            <input id="address_2" type="text"
-                                   class="form-control @error('address_2') is-invalid @enderror"
-                                   name="address_2"
-                                   value="{{ old('address_2') }}">
-
-                            @error('address_2')
-                            <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label for="district" class="col-md-4 col-form-label text-md-right">
-                            District
-                            <span class="mendatory-mark">*</span></label>
-                        <div class="col-md-6">
-                            <select name="district"
-                                    class="form-control @error('district') is-invalid @enderror"
-                                    autofocus>
-                                <option disabled selected value="">
-                                    location
-                                </option>
-                                @foreach (allDistricts() as $district)
-                                    <option value="{{$district['name']}}" @if(old('district') &&
-                                        $district['name']==old('district')) selected="selected" @endif>
-                                        @if(App::getlocale()=='en')
-                                            {{$district['name']}}
-                                        @elseif(App::getlocale()=='bn')
-                                            {{$district['bn_name']}}
-                                        @endif
-                                    </option>
+                    <label for="company_name">Company Address
+                        <span class="mendatory-mark">*</span></label><br>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <select class="form-control" name="divison" id="divison">
+                                <option value="">Select Divison</option>
+                                @foreach($categories as $category)
+                                    <option value="{{$category->id}}">
+                                        {{$category->category_name}}</option>
                                 @endforeach
                             </select>
-                            @error('district')
+                        </div>
+                        <div class="form-group col-md-6">
+                            <select class="form-control" name="district" id="district">
+                                <option value="">Select District</option>
+                                @foreach($categories as $category)
+                                    <option value="{{$category->id}}">
+                                        {{$category->category_name}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <textarea name="address" class="form-control" id="address" rows="2"
+                                  placeholder="Write Company Address"></textarea>
+                        @error('address')
+                        <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                        @enderror
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="trade_license">Business/ Trade License No
+                                <span class="mendatory-mark">*</span></label>
+                            <input id="trade_license" type="text"
+                                   class="form-control @error('trade_license') is-invalid @enderror "
+                                   name="trade_license"
+                                   value="{{ old('trade_license') }}" autofocus>
+                            @error('trade_license')
                             <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
                             @enderror
-
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="rl_no">RL No (Only for Recruiting Agency)
+                                <span class="mendatory-mark">*</span></label>
+                            <input id="rl_no" type="text"
+                                   class="form-control @error('rl_no') is-invalid @enderror"
+                                   name="rl_no" value="{{ old('rl_no') }}">
+                            @error('rl_no')
+                            <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
                     </div>
+                    <div class="form-group">
+                        <label for="desciption">Business Description
+                            <span class="mendatory-mark">*</span></label>
+                        <textarea type="text" name="desciption" rows="2" id="desciption"
+                                  class="form-control"></textarea>
+                        @error('desciption')
+                        <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                        @enderror
+                    </div>
+                    <div class="form-group ">
+                        <label for="website_url">Website URL
+                            <span class="mendatory-mark">*</span></label>
+                        <input type="text" name="website_url" id="website_url" class="form-control">
+                        @error('website_url')
+                        <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                        @enderror
+                    </div>
+                    <hr class="mt-4">
+                    <h4 class="mb-3">Contact Details</h4>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="contact_name">Contact Person's Name
+                                <span class="mendatory-mark">*</span></label>
+                            <input id="contact_name" type="text" placeholder="type your name"
+                                   class="form-control @error('contact_name') is-invalid @enderror"
+                                   name="contact_name" value="{{ old('contact_name') }}">
+                            @error('contact_name')
+                            <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="contact_phone">Contact Person's Phone</label>
+                            <div class="input-group">
+                                <div class="input-group-append">
+                                    <div class="input-group-text">+880</div>
+                                </div>
+                                <input id="contact_phone" type="text"
+                                       class="form-control @error('contact_phone') is-invalid @enderror"
+                                       name="contact_phone" value="{{ old('contact_phone') }}"
+                                       placeholder="@lang('app.contact_ex')">
 
+                            </div>
+
+                            @error('contact')
+                            <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                            @enderror
+                        </div>
+                    </div>
+                    <hr>
                     <div class="form-group row mb-0">
-                        <div class="col-md-4 offset-md-4">
-                            <button type="submit" class=" form-control btn btn-success">
-                                <i class="la la-save"></i> @lang('Register')
+                        <div class="col-md-6 offset-md-3">
+                            <button type="submit" class="form-control btn btn-success">
+                                <i class="fa fa-save"></i> @lang('Register')
                             </button>
                         </div>
                     </div>
@@ -173,4 +203,21 @@
             </div>
         </div>
     </div>
+@endsection
+@section('page-js')
+    <script>
+        $(document).ready(function () {
+            $("#icon-click").on('click', function () {
+                $('#icon-click i').toggleClass("fa-eye-slash");
+                const pass = $('#password');
+                if (pass.attr("type") == "password") {
+                    pass.attr('type', 'text');
+                } else if (pass.attr("type") == "text") {
+                    pass.attr('type', 'password');
+
+                }
+            });
+        });
+    </script>
+
 @endsection
